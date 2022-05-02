bc3cd145c261c14f8969dca1b8c9dedc0c2563a5
Loh Siu Yin <siuyin@beyondbroadcast.com>
2022-05-02 08:22:42 +0000

    Updated event processing.

--- cmd/evstream/main.go
+++ cmd/evstream/main.go
@@ -4,22 +4,23 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"github.com/nats-io/nats.go"
 )
 
 var lst = []string{
-	"marraige proposal accepted",
-	"marraige registration witnesses invited",
-	"marraige registered",
-	"official photographer hired",
-	"wedding gown for bride selected",
-	"wedding dinner guest list finalised",
-	"wedding dinner guests invited",
-	"wedding dinner menus finalised",
-	"wedding cake selected",
-	"wedding dinner restaurant booked",
-	"seating plan finalised",
+	"01 marraige proposal accepted",
+	"02 marraige registration witnesses invited",
+	"03 marraige registered",
+	"04 official photographer hired",
+	"05 wedding gown for bride selected",
+	"06 wedding dinner guest list finalised",
+	"07 wedding dinner guests invited",
+	"08 wedding dinner menus finalised",
+	"09 wedding cake selected",
+	"10 wedding dinner restaurant booked",
+	"11 seating plan finalised",
 }
 
 var (
@@ -33,6 +34,8 @@ func main() {
 
 	embedNATS()
 	defineEventStream(lst)
+
+	procEvtStream() // uncomment me
 
 	select {} // wait forever
 }
@@ -61,10 +64,12 @@ func initStream() bool {
 }
 
 func addStream() {
-
-	if _, err := js.AddStream(&nats.StreamConfig{
-		Name: "wedding", Subjects: []string{"wedding", "wedding.>"},
-	}); err != nil {
+	if _, err := js.AddStream(
+		&nats.StreamConfig{
+			Name:     "wedding",
+			Subjects: []string{"wedding", "wedding.>"},
+		},
+	); err != nil {
 		log.Fatal(err)
 	}
 }
@@ -73,4 +78,55 @@ func feedEvents(lst []string) {
 	for _, item := range lst {
 		js.PublishAsync("wedding.list", []byte(item))
 	}
+}
+
+func procEvtStream() {
+	if !weddingStreamPresent() {
+		return
+	}
+
+	go func() {
+		for {
+			msg, err := fetch()
+			if err != nil {
+				continue
+			}
+
+			fmt.Println("weddingPlanner: ", string(msg.Data))
+			msg.Ack()
+			time.Sleep(5 * time.Second) // even machine wedding planners take time
+		}
+		fmt.Println("forever loop exited. This should not happen")
+	}()
+}
+
+func weddingStreamPresent() bool {
+	_, err := js.StreamInfo("wedding")
+	if err != nil {
+		return false
+	}
+	return true
+}
+
+func fetch() (m *nats.Msg, err error) {
+	sub, err := js.PullSubscribe("wedding.list", "weddingPlanner")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	msgs, err := sub.Fetch(1)
+	if err != nil && err.Error() == "nats: timeout" {
+		return
+	}
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	if len(msgs) != 1 {
+		return m, fmt.Errorf("unexpected number of messages returned: %n", len(msgs))
+	}
+
+	return msgs[0], nil
 }
