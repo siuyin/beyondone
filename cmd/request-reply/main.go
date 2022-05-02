74bcd6882c45736b17ff063587ec169bb02b87ba
Loh Siu Yin <siuyin@beyondbroadcast.com>
2022-05-02 05:38:03 +0000

    Updated request reply demo.

--- cmd/request-reply/main.go
+++ cmd/request-reply/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"log"
+	"strconv"
+	"strings"
 	"time"
 
 	svr "github.com/nats-io/nats-server/v2/server"
@@ -21,7 +23,7 @@ func main() {
 }
 
 func embedNATS() {
-	s, err := svr.NewServer(&svr.Options{Port: 4222})
+	s, err := svr.NewServer(&svr.Options{Port: 4222, JetStream: true})
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -53,7 +55,62 @@ func waitForSvrRdy(s *svr.Server) {
 }
 
 func mathSvcStart() {
+	nc.Subscribe("math.sum", func(m *nats.Msg) {
+		replyWithSum(m)
+	})
+}
+
+func replyWithSum(m *nats.Msg) {
+	a, b, err := parseSumRequest(m)
+	if err != nil {
+		resp := fmt.Sprintf("sorry, I could not understand your request: %v", err)
+		m.Respond([]byte(resp))
+	}
+
+	m.Respond(sum(a, b))
+}
+
+func parseSumRequest(m *nats.Msg) (a, b int, err error) {
+	str := string(m.Data)
+	ss := strings.Split(str, ",")
+
+	if len(ss) != 2 {
+		err = fmt.Errorf("expected input format n1,n2: got %s", str)
+		return
+	}
+
+	a, err = strconv.Atoi(ss[0])
+	if err != nil {
+		return
+	}
+
+	b, err = strconv.Atoi(ss[1])
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+func sum(a, b int) []byte {
+	s := fmt.Sprintf("The sum of %d and %d is %d", a, b, a+b)
+	return []byte(s)
 }
 
 func requestMathSvc() {
+	go func() {
+		for {
+			var inp string
+			fmt.Println("\nEnter two numbers to be summed. eg. 2,3:")
+			fmt.Scanln(&inp)
+
+			timeout := time.Second
+			m, err := nc.Request("math.sum", []byte(inp), timeout)
+			if err != nil {
+				log.Println(err)
+			}
+
+			fmt.Printf("%s\n", m.Data)
+		}
+	}()
 }
